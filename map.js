60e62d21957057efa30b09e6c39820005c9e1c27
tckz916 <tachikaze0916@gmail.com>
2015-12-06 00:35:30 +0900

    Test

--- map.js
+++ map.js
@@ -2,40 +2,41 @@ var isStart = false;
 
 match.getWorld().on('use', function(event) {
   var player = event.getPlayer();
+  if (itemstack.getType() == 'STICK') {
+    console.log(player.getName() + "is not a STICK.");
+    return;
+  }
   if (player.getTeam() == null) {
-    console.log(player.getName() + "はチームに属していません。");
+    console.log(player.getName() + "is not in the Team.");
     return;
   }
 
   var itemstack = event.getItemStack();
 
-  if (itemstack.getType() == 'STICK') {
-    if (!checkBoolean(isStart)) {
-      isStart = true;
-      var count = 10;
-      var timer = setInterval(function() {
-        match.broadcast(count + "");
-        count--;
-        if (count < 0) {
-          var x = getRandom(-50, 50);
-          var z = getRandom(-50, 50);
-          match.getWorld().strikeLightning(x, 0, z, false);
-          clearInterval(timer);
-          isStart = false;
-        }
-      }, 1000);
-
-
-    }
+  if (!checkBoolean(isStart)) {
+    isStart = true;
+    var count = 10;
+    var timer = setInterval(function() {
+      match.broadcast(count + "");
+      count--;
+      if (count < 0) {
+        var x = getRandom(-50, 50);
+        var z = getRandom(-50, 50);
+        match.getWorld().strikeLightning(x, 0, z, false);
+        clearInterval(timer);
+        isStart = false;
+      }
+    }, 1000);
   }
+
 });
 
 function checkBoolean(val) {
-    if (val) {
-        return true;
-    } else {
-        return false;
-    }
+  if (val) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getRandom(min, max) {
