5de42e87cbc54e1a517bf64603c160688dfe3cf9
tckz916 <tachikaze0916@gmail.com>
2015-12-06 23:23:36 +0900

    Test

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ match.on('end', function() {
 match.getWorld().on('use', function(event) {
   var player = event.getPlayer();
   var itemstack = event.getItemStack();
-  var isStart = new Map();
+  var playerMap = new Object();
   if (!isGamestart) {
     return;
   }
@@ -21,28 +21,36 @@ match.getWorld().on('use', function(event) {
     return;
   }
 
-  if (!isStart.get(player.getName())) {
-    isStart.set(player.getName, true);
+  if (!playerMap.hasOwnProperty(player.getUUID())) {
+    playerMap[player.getUUID()] = false;
+  }
+
+  if (!playerMap[player.getUUID()]) {
+    playerMap[player.getUUID()] = true;
     var count = 5;
     var timer = setInterval(
       function() {
         if (count < 1) {
-          match.broadcast("`7[" + player.getTeam().getDisplayName().substring(0, 1) + player.getName() + "'s Nuke`7]: `c`lBOOM!");
+          match.broadcast("`7[" + player.getTeam().getColorCode() + player.getName() + "'s Nuke`7]: `c`lBOOM!");
           var players = match.getPlayers();
           for (var i = 0; i < players.length; i++) {
             players[i].playSound('EXPLODE', 1, 1);
             if (!(players[i].getTeam().getName() == player.getTeam().getName() || players[i].getTeam == null)) {
               players[i].kill(player);
-              match.broadcast("`7[" + player.getTeam().getDisplayName().substring(0, 1) + player.getName() + "'s Nuke`7]: `9" + players[i].getName() + " `ewas nuked by `6" + player.getName());
+              match.broadcast("`7[" + getTeamColorCode(player) + player.getName() + "'s Nuke`7]: `9" + getTeamColorCode(players[i]) + players[i].getName() + " `ewas nuked by `6" + getTeamColorCode(player) + player.getName());
             };
           }
           clearInterval(timer);
           player.removeItem(itemstack, 1);
-          isStart.set(player.getName, false);
+          playerMap[player.getUUID()] = false;
         } else {
-          match.broadcast("`7[" + player.getTeam().getDisplayName().substring(0, 1) + player.getName() + "'s Nuke`7]: `rImpact in `c" + count);
+          match.broadcast("`7[" + getTeamColorCode(player) + player.getName() + "'s Nuke`7]: `rImpact in " + getTeamColorCode(player) + count);
         }
         count--;
       }, 1000);
   }
 });
+
+function getTeamColorCode(player) {
+  return player.getTeam().getColorCode();
+}
