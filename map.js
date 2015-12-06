2f87e52c9cb7226ec0ae780db466db5ce1d8c08f
tckz916 <tachikaze0916@gmail.com>
2015-12-06 22:43:31 +0900

    Test

--- map.js
+++ map.js
@@ -1,4 +1,3 @@
-var isStart = false;
 var isGamestart = false;
 match.on('start', function() {
   isGamestart = true;
@@ -11,45 +10,39 @@ match.on('end', function() {
 match.getWorld().on('use', function(event) {
   var player = event.getPlayer();
   var itemstack = event.getItemStack();
+  var isStart = new Map();
   if (!isGamestart) {
-    console.log("Game is not start.");
-    return;
-  }
-  if (itemstack.getType() != 'BLAZE_POWDER') {
-    console.log(player.getName() + "is not a BLAZE_POWDER.");
     return;
   }
   if (player.getTeam() == null) {
-    console.log(player.getName() + "is not in the Team.");
+    return;
+  }
+  if (itemstack.getType() != 'BLAZE_POWDER') {
     return;
   }
 
-  if (!isStart) {
-    isStart = true;
+  if (!isStart.get(player.getName())) {
+    isStart.set(player.getName, true);
     var count = 5;
     var timer = setInterval(
       function() {
         if (count < 1) {
-          match.broadcast("`7[`cPB`7]: `c`lBOOM!");
+          match.broadcast("`7[" + player.getTeam().getDisplayName().substring(0, 1) + player.getName() + "'s Nuke`7]: `c`lBOOM!");
           var players = match.getPlayers();
           for (var i = 0; i < players.length; i++) {
             players[i].playSound('EXPLODE', 1, 1);
             if (!(players[i].getTeam().getName() == player.getTeam().getName() || players[i].getTeam == null)) {
               players[i].kill(player);
-              match.broadcast("`7[`cPB`7]: `9" + players[i].getName() + " `ewas nuked by `6" + player.getName());
+              match.broadcast("`7[" + player.getTeam().getDisplayName().substring(0, 1) + player.getName() + "'s Nuke`7]: `9" + players[i].getName() + " `ewas nuked by `6" + player.getName());
             };
           }
           clearInterval(timer);
-          itemstack.setType('AIR');
-          isStart = false;
+          player.removeItem(itemstack, 1);
+          isStart.set(player.getName, false);
         } else {
-          match.broadcast("`7[`cPB`7]: `rImpact in `c" + count);
+          match.broadcast("`7[" + player.getTeam().getDisplayName().substring(0, 1) + player.getName() + "'s Nuke`7]: `rImpact in `c" + count);
         }
         count--;
       }, 1000);
   }
 });
-
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
