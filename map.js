738da12742d8ffeea2efc3105b1fe3532ff24012
tckz916 <tachikaze0916@gmail.com>
2015-12-06 23:31:18 +0900

    Test

--- map.js
+++ map.js
@@ -21,30 +21,26 @@ match.getWorld().on('use', function(event) {
     return;
   }
 
-  if (!playerMap.hasOwnProperty(player.getUUID())) {
-    playerMap[player.getUUID()] = false;
-  }
-
   if (!playerMap[player.getUUID()]) {
     playerMap[player.getUUID()] = true;
     var count = 5;
     var timer = setInterval(
       function() {
         if (count < 1) {
-          match.broadcast("`7[" + player.getTeam().getColorCode() + player.getName() + "'s Nuke`7]: `c`lBOOM!");
+          match.broadcast(getPrefix() + "`c`lBOOM!");
           var players = match.getPlayers();
           for (var i = 0; i < players.length; i++) {
             players[i].playSound('EXPLODE', 1, 1);
             if (!(players[i].getTeam().getName() == player.getTeam().getName() || players[i].getTeam == null)) {
               players[i].kill(player);
-              match.broadcast("`7[" + getTeamColorCode(player) + player.getName() + "'s Nuke`7]: `9" + getTeamColorCode(players[i]) + players[i].getName() + " `ewas nuked by `6" + getTeamColorCode(player) + player.getName());
+              match.broadcast(getPrefix() + getTeamColorCode(players[i]) + players[i].getName() + " `ewas nuked by " + getTeamColorCode(player) + player.getName());
             };
           }
           clearInterval(timer);
           player.removeItem(itemstack, 1);
           playerMap[player.getUUID()] = false;
         } else {
-          match.broadcast("`7[" + getTeamColorCode(player) + player.getName() + "'s Nuke`7]: `rImpact in " + getTeamColorCode(player) + count);
+          match.broadcast(getPrefix() + " `rImpact in " + getTeamColorCode(player) + count);
         }
         count--;
       }, 1000);
@@ -53,4 +49,8 @@ match.getWorld().on('use', function(event) {
 
 function getTeamColorCode(player) {
   return player.getTeam().getColorCode();
+}
+
+function getPrefix() {
+  return "`7[" + getTeamColorCode(player) + player.getName() + "'s Nuke`7]: ";
 }
