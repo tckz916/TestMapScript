68f93b2a5bf1d7e863e29bbf5bfad59482f6f54e
tckz916 <tachikaze0916@gmail.com>
2015-12-06 23:56:40 +0900

    Test

--- map.js
+++ map.js
@@ -1,4 +1,5 @@
 var isGamestart = false;
+var playerMap = new Object();
 match.on('start', function() {
   isGamestart = true;
 });
@@ -10,8 +11,7 @@ match.on('end', function() {
 match.getWorld().on('use', function(event) {
   var player = event.getPlayer();
   var itemstack = event.getItemStack();
-  var uuid = player.getUUID();
-  var playerMap = new Object();
+  var players = match.getPlayers();
   if (!isGamestart) {
     return;
   }
@@ -22,16 +22,13 @@ match.getWorld().on('use', function(event) {
     return;
   }
 
-  player.sendMessage(playerMap);
-
-  if (!playerMap.uuid) {
-    playerMap.uuid = true;
+  if (!playerMap[player.getUUID()]) {
+    playerMap[player.getUUID()] = true;
     var count = 5;
     var timer = setInterval(
       function() {
         if (count < 1) {
           match.broadcast(getPrefix(player) + getTeamColorCode(player) + "`lBOOM!");
-          var players = match.getPlayers();
           for (var i = 0; i < players.length; i++) {
             players[i].playSound('EXPLODE', 1, 1);
             if (!(players[i].getTeam().getName() == player.getTeam().getName() || players[i].getTeam == null)) {
@@ -41,8 +38,11 @@ match.getWorld().on('use', function(event) {
           }
           clearInterval(timer);
           player.removeItem(itemstack, 1);
-          playerMap.uuid = false;
+          playerMap[player.getUUID()] = false;
         } else {
+          for (var i = 0; i < players.length; i++) {
+            players[i].playSound("WOLF_HOWL", 1, 255);
+          }
           match.broadcast(getPrefix(player) + "`rImpact in " + getTeamColorCode(player) + count);
         }
         count--;
